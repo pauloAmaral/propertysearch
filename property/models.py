5c8df8261eb3f70b02375b1bc88973bee31e2c86
Paulo Amaral <paulo.amaral@optimorlabs.com>
2017-06-22 11:00:08 +0100

    [models] Add property model and migrations

--- property/models.py
+++ property/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Property(models.Model):
+    url = models.CharField(max_length=512, null=False, blank=False)
+    price = models.FloatField(null=False, blank=False)
+    number_of_bedrooms = models.IntegerField(null=False, blank=False)
+    is_furnished = models.BooleanField(null=False, blank=False)
+    location = models.CharField(max_length=300, null=True, blank=True)
